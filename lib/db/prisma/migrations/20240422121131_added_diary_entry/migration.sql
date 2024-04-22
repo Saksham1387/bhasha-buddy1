d876eca6cef08ee53f7164e220b13acc1a4e87a1
saksham56 <saksham1387@gmail.com>
2024-04-22 19:30:50 +0530

    cleaned-project

---
+++ lib/db/prisma/migrations/20240422121131_added_diary_entry/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "DiaryEntry" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "DiaryEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "DiaryEntry" ADD CONSTRAINT "DiaryEntry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
