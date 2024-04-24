8e483ddc6232c227ec8d818ac78f2e318ecafb12
saksham56 <saksham1387@gmail.com>
2024-04-24 21:12:14 +0530

    added private-chat

--- app/(learn)/community/page.tsx
+++ app/(learn)/community/page.tsx
@@ -1,64 +1,151 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useSocket } from "@/context/SocketProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
-interface IMessage {
-    message: string;
-    avatar: string;
-}
-
 const Community = () => {
-    const { sendMessage, messages } = useSocket(); 
-    console.log(messages, sendMessage);// UseSocket already returns the correctly typed context
-    const [message, setMessage] = useState("");
-    
-    const handleSendMessage = () => {
-        if (message.trim()) {
-            sendMessage(message.trim());
-            setMessage(""); // Clear the message input after sending
-        }
-    };
+  const { sendMessage, messages, roomMessages, joinRoom } = useSocket();
+  const [messagegloabal, setMessagGlobal] = useState("");
+  const [messageroom, setMessagRoom] = useState("");
+  const [room, setRoom] = useState("");
 
-    return (
-        <div className="flex flex-col justify-between h-screen p-5 ">
-            <h1 className="text-center text-3xl font-semibold text-neutral-600 mb-5 dark:text-zinc-300">Global Chat</h1>
-            
-            <ul className="flex-1 overflow-auto p-2 space-y-2">
-                {messages.map(({ message, avatar }, index) => (
-                    <div key={index} className="flex">
-                        <li className="bg-white p-2 rounded-lg shadow flex items-center dark:bg-slate-500">
-                            <Image 
-                                src={avatar || 'path/to/default/avatar.jpg'} // Fallback for missing avatar
-                                alt="User Avatar" 
-                                width={40} 
-                                height={40}
-                                className="rounded-full" 
-                            />
-                            <p className="ml-2 mr-5">{message}</p>
-                        </li>
-                    </div>
-                ))}
-            </ul>
+  const handleSendMessageGlobal = () => {
+    if (messagegloabal.trim()) {
+      sendMessage(messagegloabal.trim());
+      setMessagGlobal("");
+    }
+  };
+
+  const handleSendMessageRoom = () => {
+    if (messageroom.trim()) {
+      console.log(messageroom.trim(), room);
+      sendMessage(messageroom.trim(), room);
+      setMessagRoom("");
+    }
+  };
+
+  const handleFormSubmitRoom = (e: any) => {
+    e.preventDefault();
+    handleSendMessageRoom();
+  };
+
+  const handleFormSubmitGlobal = (e: any) => {
+    e.preventDefault();
+    handleSendMessageGlobal();
+  };
+
+  console.log(roomMessages, room);
+  const handleJoinRoom = () => {
+    const roomName = prompt("Enter room name:");
+    if (roomName) {
+      joinRoom(roomName);
+      setRoom(roomName);
+    }
+  };
+
+  useEffect(() => {
+    console.log(roomMessages, room);
+  }, [messages, room]);
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 flex">
+        <div className="flex-1">
+          <h1 className="text-center text-3xl font-semibold text-neutral-600 mb-5 dark:text-zinc-300">
+            Global Chat
+          </h1>
+          <ul className="overflow-auto p-2 space-y-2">
+            {messages.map(({ message, avatar }, index) => (
+              <div key={index} className="flex">
+                <li className="bg-white p-2 rounded-lg shadow flex items-center dark:bg-slate-500">
+                  <Image
+                    src={avatar || "path/to/default/avatar.jpg"}
+                    alt="User Avatar"
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                  />
+                  <p className="ml-2 mr-5">{message}</p>
+                </li>
+              </div>
+            ))}
+          </ul>
+          <form onSubmit={handleFormSubmitGlobal}>
             <div className="flex items-center">
-                <input 
-                    type="text" 
-                    value={message} // Controlled input
-                    onChange={(e) => setMessage(e.target.value)} 
-                    className="w-full md:w-1/3 p-3 bg-neutral-100 rounded-xl shadow-sm outline-none focus:border-purple-500 dark:bg-slate-800 transition-all"
-                    placeholder="Say Something..." 
-                />
-                <Button 
-                    variant="secondary"
-                    onClick={handleSendMessage} // Using the handler function
-                    className="ml-4 px-6 py-2"
-                >
-                    Send
-                </Button>
+              <input
+                type="text"
+                value={messagegloabal}
+                onChange={(e) => setMessagGlobal(e.target.value)}
+                className="w-full md:w-1/3 p-3 bg-neutral-100 rounded-xl shadow-sm outline-none focus:border-purple-500 dark:bg-slate-800 transition-all"
+                placeholder="Say Something..."
+              />
+              <Button
+                variant="secondary"
+                type="submit"
+                className="ml-4 px-6 py-2"
+              >
+                Send
+              </Button>
             </div>
+          </form>
         </div>
-    );
+
+        {/* room chat */}
+        {room && (
+          <div>
+            <div className="flex-1">
+              <h1 className="text-center text-3xl font-semibold text-neutral-600 mb-5 dark:text-zinc-300">
+                Room Chat
+              </h1>
+              <ul className="overflow-auto p-2 space-y-2">
+                {roomMessages.map(({ message, avatar }, index) => (
+                  <div key={index} className="flex">
+                    <li className="bg-white p-2 rounded-lg shadow flex items-center dark:bg-slate-500">
+                      <Image
+                        src={avatar || "path/to/default/avatar.jpg"}
+                        alt="User Avatar"
+                        width={40}
+                        height={40}
+                        className="rounded-full"
+                      />
+                      <p className="ml-2 mr-5">{message}</p>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+              <form onSubmit={handleFormSubmitRoom}>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    value={messageroom}
+                    onChange={(e) => setMessagRoom(e.target.value)}
+                    className="w-full md:w-1/3 p-3 bg-neutral-100 rounded-xl shadow-sm outline-none focus:border-purple-500 dark:bg-slate-800 transition-all"
+                    placeholder="Say Something..."
+                  />
+                  <Button
+                    variant="secondary"
+                    type="submit"
+                    className="ml-4 px-6 py-2"
+                  >
+                    Send
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex items-center justify-center">
+        <Button
+          variant="primary"
+          onClick={handleJoinRoom}
+          className="px-6 py-2"
+        >
+          Join Room
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Community;
--- context/SocketProvider.tsx
+++ context/SocketProvider.tsx
@@ -2,7 +2,6 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-
 interface SocketProviderProps {
   children?: React.ReactNode;
 }
@@ -10,11 +9,14 @@ interface SocketProviderProps {
 interface Message {
   message: string;
   avatar: string;
+  room?: string;
 }
 
 interface ISocketContext {
-  sendMessage: (msg: string) => void;
+  sendMessage: (msg: string, room?: string) => void;
   messages: Message[];
+  roomMessages: Message[];
+  joinRoom: (room: string) => void;
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
@@ -26,38 +28,65 @@ export const useSocket = () => {
 };
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
-  // const { user } = useUser();
-  const defaultAvatar = '/gamer.png'; // Fallback to a default avatar if necessary
+  const defaultAvatar = "/gamer.png";
   const [socket, setSocket] = useState<Socket>();
   const [messages, setMessages] = useState<Message[]>([]);
-    //@ts-ignore
-  const sendMessage = useCallback((msg) => {
-    if (socket && msg.trim().length > 0) {
-      const avatar =  defaultAvatar;
-      socket.emit('event:message', { message: msg, avatar: avatar });
-    }
-  }, [socket, ]);
+  const [roomMessages, setRoomMessages] = useState<Message[]>([]);
+  //@ts-ignore
+  const sendMessage = useCallback(
+    (msg: string, room?: string) => {
+      if (socket && msg.trim().length > 0) {
+        const avatar = defaultAvatar;
 
-  const onMessageRec = useCallback((msg: string) => {
-    const data = JSON.parse(msg);
-    if (data.message && data.avatar) {
-      setMessages(prev => [...prev, data]);
+        if (room) {
+          socket.emit("event:message", { room, message: msg, avatar: avatar });
+        } else {
+          socket.emit("event:message", { message: msg, avatar: avatar });
+        }
+      }
+    },
+    [socket, defaultAvatar]
+  );
+
+  const joinRoom = useCallback(
+    (room: string) => {
+      if (socket) {
+        socket.emit("joinRoom", room);
+      }
+    },
+    [socket]
+  );
+
+  const onMessageRec = useCallback((data: Message) => {
+    console.log(data);
+    if (data.room) {
+      console.log("inside onMessageRec for room ");
+      setRoomMessages((prev) => [...prev, data]);
+      console.log(roomMessages);
+    } else {
+      console.log("inside onMessageRec for global ");
+      setMessages((prev) => [...prev, data]);
     }
   }, []);
 
   useEffect(() => {
-    const _socket = io('http://localhost:8000');
-    _socket.on('message', onMessageRec);
+    //@ts-ignore
+    const _socket = io(process.env.NEXT_PUBLIC_URL_SOCKET);
+    _socket.on("room-message", onMessageRec);
+    _socket.on("message", onMessageRec);
     setSocket(_socket);
 
     return () => {
       _socket.disconnect();
-      _socket.off('message', onMessageRec);
+      _socket.off("room-message", onMessageRec);
+      _socket.off("message", onMessageRec);
     };
   }, []);
 
   return (
-    <SocketContext.Provider value={{ sendMessage, messages }}>
+    <SocketContext.Provider
+      value={{ sendMessage, messages, roomMessages, joinRoom }}
+    >
       {children}
     </SocketContext.Provider>
   );
